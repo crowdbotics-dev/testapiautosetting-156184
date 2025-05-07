0b105e085e8585ab57919185b9b2a88ed398fc5f
Crowdbotics <engineering@crowdbotics.com>
2025-05-07 10:07:19 +0000

    Update admin title

--- backend/testapiautosetting_156184/urls.py
+++ backend/testapiautosetting_156184/urls.py
@@ -34,9 +34,9 @@ urlpatterns = [
     path("rest-auth/registration/", include("dj_rest_auth.registration.urls")),
 ]
 
-admin.site.site_header = "TestAPIAutoSettingst21xw"
-admin.site.site_title = "TestAPIAutoSettingst21xw Admin Portal"
-admin.site.index_title = "TestAPIAutoSettingst21xw Admin"
+admin.site.site_header = "TestAPIAutoSettingst21xw1"
+admin.site.site_title = "TestAPIAutoSettingst21xw1 Admin Portal"
+admin.site.index_title = "TestAPIAutoSettingst21xw1 Admin"
 
 # swagger
 urlpatterns += [
